b8dc376ba4b9edecd7fbfe2fdbcddb4bb194ad19
Patrick Mahoney <patrick@sitesee.io>
2020-02-19 12:01:57 +1000

    Fixed P3 conversion to match original

--- paper-more-button.js
+++ paper-more-button.js
@@ -1,29 +1,45 @@
-Polymer({
-		is: 'paper-more-button',
+import { PolymerElement, html } from '@polymer/polymer';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-spinner/paper-spinner-lite.js';
 
-    properties: {
-	    	/**
-	    	 * True if the lister has more items than currently shown
-	    	 */
-        hasMore: {
-            type: Boolean,
-            value: false
-        },
+class PaperMoreButton extends PolymerElement {
+    static get template() {
+        return html`
+<paper-button hidden$=[[!_showMore(hasMore,loading)]] on-tap="_tapMore"><slot></slot></paper-button>
+</paper-spinner-lite>`;
+    }
 
-	    	/**
-	    	 * True if the lister is currently loading items
-	    	 */
-        loading: {
-            type: Boolean,
-            value: false
-        }
-		},
-		
-		// Private methods
-		_tapMore: function() {
-        this.fire('tap-more');
-		},
-		_showMore: function(hasMore, loading) {
-        return hasMore && !loading;
-		}
-});
+    static get is() {
+        return 'paper-more-button';
+    }
+
+    static get properties() {
+        return {
+			/**
+			 * True if the lister has more items than currently shown
+			 */
+			hasMore: {
+				type: Boolean,
+				value: false
+			},
+
+			/**
+			 * True if the lister is currently loading items
+			 */
+			loading: {
+				type: Boolean,
+				value: false
+			}
+		};
+    }
+
+	// Private methods
+	_tapMore() {
+		this.dispatchEvent(new CustomEvent('tap-more', {detail: {kicked: true}}));
+	}
+	_showMore(hasMore, loading) {
+		return hasMore && !loading;
+	}
+}
+
+customElements.define(PaperMoreButton.is, PaperMoreButton);
